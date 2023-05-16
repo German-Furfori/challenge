9b60cf311fb3be235f8e2b0c66d4442c6ac3f76f
German <german.furfori@gmail.com>
2023-05-15 23:26:39 -0300

    Proyecto creado, con la entidad y primer prueba con la consola H2

---
+++ src/main/java/com/paygoal/app/entities/ProductEntity.java
@@ -0,0 +1,31 @@
+package com.paygoal.app.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String descripcion;
+
+    private Double precio;
+
+    private Integer cantidad;
+
+}
